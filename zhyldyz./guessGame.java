a7aee9c503e24318ff2ba9dea285f99b2db29d80
KYDYRBAEVA <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-09-15 08:57:45 +0600

    Update guessGame.java

--- zhyldyz./guessGame.java
+++ zhyldyz./guessGame.java
@@ -1,15 +1,17 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.*;
 public class GuessingGame {
 
     public static void main(String[] args) {
         System.out.println("Let's play a game.");
         System.out.println("1 and 100.");
-        boolean playAgain;
+	Scanner game = new Scanner(System.in );
+	int max;   
+        max = inputnum.nextInt();
         do {
             playGame();  
             System.out.print("Would you like to play again? ");
-            playAgain = TextIO.getlnBoolean();
         } while (playAgain);
         System.out.println("Thanks for playing.  Goodbye.");
     }            
@@ -17,30 +19,30 @@ public class GuessingGame {
     
     
     static void playGame() {
-        int computersNumber; 
+        int comNumber; 
         int usersGuess;     
         int guessCount;    
-        computersNumber = (int)(100 * Math.random()) + 1;
+        comNumber = (int)(100 * Math.random()) + 1;
         guessCount = 0;
         System.out.println();
         System.out.print("What is your first guess? ");
         while (true) {
             usersGuess = TextIO.getInt();
             guessCount++;
-            if (usersGuess == computersNumber) {
+            if (usersGuess == comNumber) {
                 System.out.println("You got it in " + guessCount
-                        + " guesses!  My number was " + computersNumber);
+                        + " guesses!  My number was " + comNumber);
                 break;  
             }
             if (guessCount == 6) {
                 System.out.println("You didn't get the number in 6 guesses.");
-                System.out.println("You lose.  My number was " + computersNumber);
+                System.out.println("You lose.  My number was " + comNumber);
                 break; 
             }
         
-            if (usersGuess < computersNumber)
+            if (usersGuess < comNumber)
                 System.out.print("That's too low.  Try again: ");
-            else if (usersGuess > computersNumber)
+            else if (usersGuess > comNumber)
                 System.out.print("That's too high.  Try again: ");
         }
         System.out.println();
