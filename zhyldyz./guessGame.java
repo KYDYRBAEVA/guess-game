73b1aa9d13a59134e31502d50eb89eb4c7d9e402
KYDYRBAEVA <38517058+KYDYRBAEVA@users.noreply.github.com>
2018-09-15 09:25:39 +0600

    Update guessGame.java

--- zhyldyz./guessGame.java
+++ zhyldyz./guessGame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.util.*;
-public class GuessingGame {
+public class GuessNumber {
 
     public static void main(String[] args) {
         System.out.println("Let's play a game.");
